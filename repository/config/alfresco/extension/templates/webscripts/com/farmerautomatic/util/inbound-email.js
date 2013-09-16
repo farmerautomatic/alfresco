32f46df56aac59308a38c8b8d08b8213f7871d95
drq <alfresco@farmerautomatic.com>
2013-09-15 20:32:51 -0400

    Enhanced scripted with archiving capabilities.

--- repository/config/alfresco/extension/templates/webscripts/com/farmerautomatic/util/inbound-email.js
+++ repository/config/alfresco/extension/templates/webscripts/com/farmerautomatic/util/inbound-email.js
@@ -5,7 +5,7 @@ function process_inbound_email(document)
     if (document.hasAspect("cm:emailed"))
     {
         // Get email subject line
-        var subjectLine = document.properties["cm:subjectline"];
+        var subjectLine = document.properties["cm:subjectline"].toLowerCase();
         logger.log("Incoming document subject line is " + subjectLine);
         if (subjectLine != null)
         {
@@ -17,14 +17,84 @@ function process_inbound_email(document)
                 var newCats = new Array();
                 for (var i = 0, len = fmCategories.length; i < len; i ++)
                 {
-                    if (subjectLine.indexOf(fmCategories[i].properties["cm:name"]) != -1)
+                    if (subjectLine.indexOf(fmCategories[i].properties["cm:name"].toLowerCase()) != -1)
                     {
                         newCats.push(fmCategories[i].nodeRef);
-                        logger.log("Added matched category " + fmCategories[i].properties["cm:name"] + "("  + fmCategories[i].nodeRef + ")" );
+                        logger.log("Added matched category " + fmCategories[i].properties["cm:name"] + " (" + fmCategories[i].nodeRef + ")");
                     }
                 }
                 document.properties["cm:categories"] = newCats;
                 document.save();
+            }
+            var currentDate = new Date();
+            var currentMonthStrPadding = "";
+            var currentDayStrPadding = "";
+
+            if (currentDate.getMonth() < 9)
+            {
+                currentMonthStrPadding = "0";
+            }
+            if (currentDate.getDate() < 10)
+            {
+                currentDayStrPadding = "0";
+            }
+            var currentDateStr = currentMonthStrPadding + (currentDate.getMonth() + 1) + "-" + currentDayStrPadding + currentDate.getDate() + "-" + currentDate.getFullYear();
+            var currentTimeStr = currentDate.getHours() + "-" + currentDate.getMinutes() + "-" + currentDate.getSeconds();
+
+            // Run transformation
+            var pdfDoc = document.transformDocument("application/pdf");
+            logger.log("Transformed document to PDF" + pdfDoc.properties["cm:name"]);
+
+            // Grab the archives folder
+            var parentName = document.parent.properties["cm:name"];
+
+            var archives = document.parent.parent.childByNamePath(parentName + " Archives");
+
+            if (archives == null)
+            {
+                archives = document.parent.parent.childByNamePath("Archives");
+            }
+
+            if (archives != null)
+            {
+                logger.log("Found the archives folder.");
+                // Locate my archive folder
+                var originator = document.properties["cm:originator"];
+                var myArchives = archives.childByNamePath(originator);
+                if (myArchives == null)
+                {
+                    // Create archive sub-folder
+                    myArchives = archives.createFolder(originator);
+                }
+                // Create a yyyy-mm-dd sub-folder
+
+                var subSpace = myArchives.childByNamePath(currentDateStr);
+                if (subSpace == null)
+                {
+                    subSpace = myArchives.createFolder(currentDateStr);
+                }
+                logger.log("Created sub space " + subSpace.properties["cm:name"]);
+                // Create a hour-minute-second sub-sub-folder
+                var subSubSpace = subSpace.childByNamePath(currentTimeStr);
+                if (subSubSpace == null)
+                {
+                    subSubSpace = subSpace.createFolder(currentTimeStr);
+                }
+                logger.log("Created sub-sub space " + subSubSpace.properties["cm:name"]);
+                // Move the documents over
+                document.move(subSubSpace);
+                logger.log("Document moved.");
+
+                var attachments = document.assocs["cm:attachments"];
+                if (attachments != null)
+                {
+                    for (var j = 0; j < attachments.length; j ++)
+                    {
+                        var attachment = attachments[j];
+                        attachment.move(subSubSpace);
+                        logger.log("Document moved.");
+                    }
+                }
             }
         }
     }
